9163fee1f50a4168692ec2de25b4adc4f04ca0e0
ukgt <ukgt2019@gmail.com>
2019-01-15 18:41:43 -0500

    modal items added

--- mila.js
+++ mila.js
@@ -1,28 +1,49 @@
  $(document).ready(function(){
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+       modal.style.display = "block";
+     }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // // When the user clicks anywhere outside of the modal, close it
+   //  window.onclick = function(event) {
+   //    if (event.target == modal) {
+   //      modal.style.display = "none";
+   //    }
+   //  }
     $('#btnSignUp').on("click", function(event){
        event.preventDefault();
        
+
+
+
        const email = $('input[type = "email"]').val();
        const password = $('input[type = "password"]').val();
        console.log('Email', email);
        console.log ('Password', password);
       
 
-       
-
-// firebase.auth().signInWithEmailAndPassword('email', 'password')
-// .then (function(data){
-//    console.log('Data', data);
-// })
-// .catch(function(error){
-//    console.log('error', error);
-// });
 firebase.auth().createUserWithEmailAndPassword(email, password)
 .then (function(data){
    console.log('Data', data);
 })
 .catch(function(error){
    console.log('Error', error);
+
+
 });
 
 // $('form').get(0).reset();
--- signin.js
+++ signin.js
@@ -12,6 +12,9 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
      var errorCode = error.code;
      var errorMessage = error.message;
      // ...
+    
+
+     });
    });
    firebase.auth().onAuthStateChanged(function(user) {
      if (user) {
@@ -22,5 +25,33 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
        // No user is signed in.
      }
    });
-});
+
+   
+ 
+  //  $("#btnLogIn").on("click", function(){
+  //   modal.style.display = "block";
+  var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+ modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+ modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+ if (event.target == modal) {
+   modal.style.display = "none";
+}
+};
 });
