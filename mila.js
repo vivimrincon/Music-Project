fcbd1fe1b73fb73484f361e30a086ddd87e5716c
ukgt <ukgt2019@gmail.com>
2019-01-15 22:01:22 -0500

    password weak message working

--- mila.js
+++ mila.js
@@ -1,44 +1,42 @@
- $(document).ready(function(){
-       $('#btnSignUp').on("click", function(event){
+ $(document).ready(function () {
+    $('#btnSignUp').on("click", function (event) {
        event.preventDefault();
-       
-
-
-
        const email = $('input[type = "email"]').val();
        const password = $('input[type = "password"]').val();
        console.log('Email', email);
-       console.log ('Password', password);
-      
-
-firebase.auth().createUserWithEmailAndPassword(email, password)
-.then (function(data){
-   console.log('Data', data);
-})
-.catch(function(error){
-    console.log('Error', error);
-modal();
-});
-function modal() {
-   console.log("did I work inside the trigger");
-   $("#exampleModal").modal ("show")
-
-console.log("did I work?");
-}
-
-// $('form').get(0).reset();
-
-dataRef().ref().push({
-   email: email,
-   password: password
-});
-dataRef().ref().on('child_added', function(addRow){
-let email = addRow.val().email;
-let password = addRow.val().password;
-
-});
+       console.log('Password', password);
 
 
-});
+       firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(function (data) {
+             console.log('Data', data);
+          })
+          .catch(function (error) {
+             console.log('Error', error);
+            // error.message
+            console.log("message", error.message);
+             modal();
 
-});
+             $('#exampleModalLabel').html( error.message);
+          });
+
+       function modal() {
+          $("#exampleModal").modal("show")
+       }
+
+       // $('form').get(0).reset();
+
+       dataRef().ref().push({
+          email: email,
+          password: password
+       });
+       dataRef().ref().on('child_added', function (addRow) {
+          let email = addRow.val().email;
+          let password = addRow.val().password;
+
+       });
+
+
+    });
+
+ });
