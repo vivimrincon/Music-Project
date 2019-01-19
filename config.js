d3abc2b23574aa5a3cf543d35aa695514a24c0d1
ttalgihon <vivithefool@gmail.com>
2019-01-19 10:33:55 -0500

    expose config

---
+++ config.js
@@ -0,0 +1,5 @@
+// TOKEN on has access for 60minutes. until you have to generate a new TOKEN.
+const TOKEN = 'BQCfj4ecstJJiDxKr976g8pInEU3eCW_-J8p9xxdCEDv_wY9U3wLImtRg-3hrmV8Wz4-mI_GdlQ8gBH69Kb2CiiZ9QNGtAs2fpCyi9YYj1bfivuh5zCp1UbFK8S1Xzy5AqxI-GDrmDfp4FXJmJ4NnJpH-ggit'; //search token
+const CLIENTID = '1b20fe021bbf4664894a68f046f7f941';
+const CLIENTSECRET = '3742b5a94bd441ec8c0152d504b6614a';
+const URI = 'https://ttalgihon.github.io/Music-Project/'
