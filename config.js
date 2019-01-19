664f37da9bca3defd20fdcc99be0f258b10f7228
ttalgihon <31228739+ttalgihon@users.noreply.github.com>
2019-01-19 10:43:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // TOKEN on has access for 60minutes. until you have to generate a new TOKEN.
-const TOKEN = 'BQCfj4ecstJJiDxKr976g8pInEU3eCW_-J8p9xxdCEDv_wY9U3wLImtRg-3hrmV8Wz4-mI_GdlQ8gBH69Kb2CiiZ9QNGtAs2fpCyi9YYj1bfivuh5zCp1UbFK8S1Xzy5AqxI-GDrmDfp4FXJmJ4NnJpH-g'; //search token
+const TOKEN = 'BQA_oUbPqHxUqVm5Ha5mWus3ghpHdJ2nvgSUk1paaD32AwVZbKSb6qja53pTXzDYvwnRvtqNtBy_3C6MSg9HBAbyM4Q2MH3-itgdOlQ-seVeboINLC5g74u-7O0PE4dgM83BGBQprmi_C5ecgFliLKbccw'; //search token
 const CLIENTID = '1b20fe021bbf4664894a68f046f7f941';
 const CLIENTSECRET = '3742b5a94bd441ec8c0152d504b6614a';
 const URI = 'https://ttalgihon.github.io/Music-Project/'
