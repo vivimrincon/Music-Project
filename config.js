fd2f3cbda25bf3357ed5fff22028a1f57cf65ae9
ttalgihon <31228739+ttalgihon@users.noreply.github.com>
2019-01-22 17:23:53 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// TOKEN on has access for 60minutes. until you have to generate a new TOKEN.
-const TOKEN = 'BQA_oUbPqHxUqVm5Ha5mWus3ghpHdJ2nvgSUk1paaD32AwVZbKSb6qja53pTXzDYvwnRvtqNtBy_3C6MSg9HBAbyM4Q2MH3-itgdOlQ-seVeboINLC5g74u-7O0PE4dgM83BGBQprmi_C5ecgFliLKbccw'; //search token
-const CLIENTID = '1b20fe021bbf4664894a68f046f7f941';
-const CLIENTSECRET = '3742b5a94bd441ec8c0152d504b6614a';
-const URI = 'https://ttalgihon.github.io/Music-Project/'
